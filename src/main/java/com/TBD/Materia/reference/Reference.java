a861241e5f820d95927ceef21bea716934e1683c
TheBloodDivision <theblooddivision@gmail.com>
2015-01-25 00:18:37 +0100

    Reference setup

---
+++ src/main/java/com/TBD/Materia/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.TBD.Materia.reference;
+
+public class Reference {
+    public static final String MOD_ID = "Materia";
+    public static final String MOD_NAME = "Materia";
+    public static final String VERSION = "1.7.10-0.5";
+    public static final String PROXY_CLIENT = "com.TBD.Materia.proxy.ClientProxy";
+    public static final String PROXY_SERVER = "com.TBD.Materia.proxy.ServerProxy";
+}
